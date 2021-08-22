bb606e76a2c0526aaffe0571bfde570b389a2759
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-08-22 21:49:19 +0530

    Create userbean.java

---
+++ fetching values from bean file/userbean.java
@@ -0,0 +1,24 @@
+package com.jstl.telusko;
+
+public class userbean 
+{
+	private int id;
+	private String name;
+	public userbean(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
